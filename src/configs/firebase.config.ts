3ef5d3a54b47cd0e3d4f52adb2076138a0e60b4b
Clément <clement.bilger25@gmail.com>
2025-08-18 01:20:57 +0200

    adding pr-validation.yml

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -2,54 +2,36 @@ import * as admin from 'firebase-admin';
 import * as fs from 'fs';
 import * as path from 'path';
 
-// Éviter l'initialisation Firebase en mode test
-let firebase: any;
+let firebaseAccount: any;
 
-if (process.env.NODE_ENV === 'test') {
-  // Mock pour les tests
-  firebase = {
-    auth: () => ({
-      verifyIdToken: jest.fn(),
-      getUser: jest.fn(),
-    }),
-  };
+const secretPath = '/etc/secrets/credentials.json';
+const localPath = path.resolve(process.cwd(), './src/configs/credentials.json');
+
+if (fs.existsSync(secretPath)) {
+  // 👉 Environnement Render
+  firebaseAccount = JSON.parse(fs.readFileSync(secretPath, 'utf8'));
+} else if (fs.existsSync(localPath)) {
+  // 👉 Environnement local
+  firebaseAccount = JSON.parse(fs.readFileSync(localPath, 'utf8'));
 } else {
-  let firebaseAccount: any;
-
-  const secretPath = '/etc/secrets/credentials.json';
-  const localPath = path.resolve(
-    process.cwd(),
-    './src/configs/credentials.json',
-  );
-
-  if (fs.existsSync(secretPath)) {
-    // 👉 Environnement Render
-    firebaseAccount = JSON.parse(fs.readFileSync(secretPath, 'utf8'));
-  } else if (fs.existsSync(localPath)) {
-    // 👉 Environnement local
-    firebaseAccount = JSON.parse(fs.readFileSync(localPath, 'utf8'));
-  } else {
-    throw new Error(
-      '❌ credentials.json introuvable dans aucun environnement.',
-    );
-  }
-
-  const firebaseParams = {
-    type: firebaseAccount.type,
-    projectId: firebaseAccount.project_id,
-    privateKeyId: firebaseAccount.private_key_id,
-    privateKey: firebaseAccount.private_key,
-    clientEmail: firebaseAccount.client_email,
-    clientId: firebaseAccount.client_id,
-    authUri: firebaseAccount.auth_uri,
-    tokenUri: firebaseAccount.token_uri,
-    authProviderX509CertUrl: firebaseAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: firebaseAccount.client_x509_cert_url,
-  };
-
-  firebase = admin.initializeApp({
-    credential: admin.credential.cert(firebaseParams),
-  });
+  throw new Error('❌ credentials.json introuvable dans aucun environnement.');
 }
+
+const firebaseParams = {
+  type: firebaseAccount.type,
+  projectId: firebaseAccount.project_id,
+  privateKeyId: firebaseAccount.private_key_id,
+  privateKey: firebaseAccount.private_key,
+  clientEmail: firebaseAccount.client_email,
+  clientId: firebaseAccount.client_id,
+  authUri: firebaseAccount.auth_uri,
+  tokenUri: firebaseAccount.token_uri,
+  authProviderX509CertUrl: firebaseAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: firebaseAccount.client_x509_cert_url,
+};
+
+const firebase = admin.initializeApp({
+  credential: admin.credential.cert(firebaseParams),
+});
 
 export default firebase;
